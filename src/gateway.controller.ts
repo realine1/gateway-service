849c922be1b6c00262ef33affa7676b05e2b6f08
Yono <yono@ideku.id>
2025-01-30 15:22:45 +0700

    create, getAll, getByUsername

--- src/gateway.controller.ts
+++ src/gateway.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, ConflictException, Controller, Get, Param, Post } from '@nestjs/common';
 import { GatewayService } from './gateway.service';
+import { CreateUserDto } from './users/dto/create-user.dto';
 
 @Controller('users')
 export class GatewayController {
   constructor(private readonly gatewayService: GatewayService) {}
 
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    const user = await this.gatewayService.createUser(createUserDto);
+    return user;
+  }
+
   @Get()
-    async getUsers() {
-        return { message: 'Hello from Gateway' };
-    }
+  async getALlUser() {
+    const users = await this.gatewayService.getAllUser();
+    return users;
+  }
 
   @Get(':username')
   async getUser(@Param('username') username: string) {
--- src/gateway.service.ts
+++ src/gateway.service.ts
@@ -1,18 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Client, ClientTCP, Transport } from '@nestjs/microservices';
+import { CreateUserDto } from './users/dto/create-user.dto';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class GatewayService {
   @Client({
     transport: Transport.TCP,
     options: {
-      host: 'localhost',  // Docker container name for User-Service
-      port: 3002,            // Port User-Service is listening on for TCP requests
+      host: 'localhost', // Docker container name for User-Service
+      port: 3002, // Port User-Service is listening on for TCP requests
     },
   })
   private client: ClientTCP;
 
   async getUserByUsername(username: string) {
     return this.client.send({ cmd: 'get-user' }, username);
+  }
+
+  async createUser(createUserDto: CreateUserDto) {
+    try {
+      const result = await lastValueFrom(
+        this.client.send({ cmd: 'create-user' }, createUserDto)
+      );
+      return result;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  async getAllUser() {
+    return this.client.send({ cmd: 'get-all-user' }, {});
   }
 }
